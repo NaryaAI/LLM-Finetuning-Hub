4ccd337680924714628762dec55b48e3e836acf9
mariia.ponomarenko@georgian.io <mariia.ponomarenko@georgian.io>
2023-10-18 02:45:50 -0400

    fixed most of the comments

--- inference/automated_benchmark/run_inference.py
+++ inference/automated_benchmark/run_inference.py
@@ -3,22 +3,20 @@ import argparse
 from pathlib import Path
 import click 
 import sys 
+import typer
 
-@click.command()
-@click.option("--huggingface_token", prompt=True, help="Your Hugging Face token")
-@click.option("--huggingface_username", default="mariiaponom", prompt=True, help="Your Hugging Face username")
-@click.option("--model_name", default="llama_7b_class", prompt=True, help="Model name (e.g. llama-7b-class): ")
-@click.option("--model_type", default="llama", prompt=True, help="Model type (llama, flan, falcon, red_pajama):")
-@click.option("--task", default="classification", prompt=True, help="Task (classification, summarization): ")
-@click.option("--compute", default="a10", prompt=True, help="Compute type (a100, a10): ")
-@click.option("--server", default="ray", prompt=True, help="Compute type (tgi, ray, triton, vllm): ")
-@click.option("--lora_weights", default="./models/llama-7b-class", prompt=True, help="Lora weights")
-def main(huggingface_token, huggingface_username, model_name, model_type, task, compute, server, lora_weights):
-    huggingface_repo = f"{huggingface_username}/{model_name}"
+def main():
+  
+    huggingface_token = typer.prompt("HuggingFace token")
+    huggingface_repo = typer.prompt("HuggingFace repository")
+    model_type = typer.prompt("Model type")
+    task = typer.prompt("Task")
+    server = typer.prompt("Server")
+    path_to_lora_weights = typer.prompt("Path to lora weights")
+
     subprocess.run(["chmod", "+x", f"./script_inference.sh"])
     subprocess.run([f"./script_inference.sh", f"{huggingface_repo}", huggingface_token, 
-                                        model_type, task, compute, 
-                                        server, lora_weights])
+                                        model_type, task, server, path_to_lora_weights])
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
